8f2ac464c6a0b72c6386d0358d5faeabe2c65344
danylagacione <danielycrys24@gmail.com>
2021-05-24 17:50:07 -0300

    adicionado configuracoes do bd postgres

--- tsdjango/settings.py
+++ tsdjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'tsdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'techstart',
+        'PASSWORD':'techstart2d',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
